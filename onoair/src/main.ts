1f98f3ddf6fbf7cff8124da36e8814d56c85ded5
OmriKadouri97 <144044822+OmriKadouri97@users.noreply.github.com>
2025-01-16 17:47:25 +0200

    try to fix the code 6

--- onoair/src/main.ts
+++ onoair/src/main.ts
@@ -4,4 +4,10 @@ import { AppComponent } from './app/app.component';
 
 platformBrowserDynamic()
   .bootstrapComponent(AppComponent)
-  .catch((err: unknown) => console.error('Error bootstrapping:', err));
+  .catch((err: unknown) => {
+    if (err instanceof Error) {
+      console.error('Error bootstrapping the application:', err.message);
+    } else {
+      console.error('Unexpected error:', err);
+    }
+  });
