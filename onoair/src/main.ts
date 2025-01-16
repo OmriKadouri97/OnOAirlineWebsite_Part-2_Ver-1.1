64bf3abc8d56680c09c37445e7710f4049a6b86a
OmriKadouri97 <144044822+OmriKadouri97@users.noreply.github.com>
2025-01-16 18:37:42 +0200

    Now all the problems are fixed

--- onoair/src/main.ts
+++ onoair/src/main.ts
@@ -1,13 +1,6 @@
-// src/main.ts
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic()
-  .bootstrapComponent(AppComponent)
-  .catch((err: unknown) => {
-    if (err instanceof Error) {
-      console.error('Error bootstrapping the application:', err.message);
-    } else {
-      console.error('Unexpected error:', err);
-    }
-  });
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
